137d73c260d1e166a61fb67bc787b73c3381cbad
mansi <vaghasiyamaahigmail.com>
2024-02-20 08:13:57 +0530

    20/02/2024

---
+++ 7.function/basicFun.js
@@ -0,0 +1,65 @@
+// function -> perform to specific
+
+//hoisting
+
+// square('10' , '5');     //function call
+
+// definition
+
+// function square(a , b){
+//     console.log(a*b);
+// }
+
+
+// basic function
+
+// function add(a , b)
+// {
+//     return a + b;
+// }
+
+// console.log(add(5,15));
+// let z = add(10,15);
+// console.log(z);
+
+
+// anonymous function         /function as expression
+
+// let product = function(x , y){
+//     return x * y;
+// };
+
+// console.log(product(11,2));
+
+// IIFE
+// let x = (function hello(a , b)
+// {
+//     return(a - b);
+// })("10" , 15);
+// console.log(x);
+
+// (function hello()
+// {
+//     console.log("hello World");
+// })();
+
+
+// arrow function
+
+// function add(a,b)
+// {
+//     return a + b;
+// }
+// console.log(add(10,5));
+
+// let add = (a,b) => {
+//     return a+b;
+// }
+// let add = (a,b) => a+b;
+// console.log(add(20,30));
+
+
+// function construction
+
+// let hello = new function("a" , "b" , "return a*b");
+// console.log(hello(12 , 5));
